71e61ea9497fb1eadd5cb5c21ba17729e0b42a76
Scott Frechette <scottfrechette@gmail.com>
2019-04-20 23:21:16 -0500

    Upgrade app to 2.0 with new tabs and better formatting.

--- got/app.R
+++ got/app.R
@@ -1,83 +1,198 @@
 
 
 library(shiny)
-# library(googlesheets)
 library(tidyverse)
+library(plotly)
+# library(gghighlight)
 
 load(here::here("got", "got-data.RData"))
 
+last_updated <- file.mtime(here::here("got", "got-data.RData")) %>% 
+    format("%m/%d/%y %I:%M %p")
 
-# answers <- gs_title("GOT") %>% 
-#     gs_read(ws = 1) %>% 
-#     rename(Team = `How do you know me?`) %>% 
-#     select(-Timestamp, -`Email Address`)
-# 
-# points <- gs_title("GOT") %>% 
-#     gs_read(ws = 2) %>% 
-#     filter(correct == "yes")
-# 
-# got <- answers %>% 
-#     gather(question, answer,
-#            -Name, -Team) %>% 
-#     separate_rows(answer, sep = ",") %>% 
-#     left_join(points, by = c("question", "answer")) %>%
-#     mutate(points = case_when(
-#         answer %in% c("Die", "Live",
-#                       "Reanimated/Turned", 
-#                       "Not Seen/Unknown") ~ 10,
-#         !str_detect(answer, "\\d") ~ 50,
-#         TRUE ~ str_extract(answer, "(?<=\\().*(?=\\))") %>% # fix this 
-#                str_extract("\\d*") %>% 
-#                as.numeric))
-# 
-# points_earned <- got %>% 
-#     filter(correct == "yes") %>% 
-#     count(Name, Team, 
-#           wt = points, name = "Points")
-# 
-# points_remaining <- got %>% 
-#     filter(is.na(correct)) %>% 
-#     count(Name, Team, 
-#           wt = points, name = "PPR")
-# 
-# got_points <- full_join(points_earned, 
-#                         points_remaining,
-#                         by = c("Name", "Team")) %>% 
-#     replace_na(list(Points = 0,
-#                     PPR = 0)) %>% 
-#     arrange(-Points)
+reorder_within <- function(x, by, within, fun = mean, 
+                           sep = "___", ...) {
+    new_x <- paste(x, within, sep = sep)
+    stats::reorder(new_x, by, FUN = fun)
+}
+scale_x_reordered <- function(..., sep = "___") {
+    reg <- paste0(sep, ".+$")
+    ggplot2::scale_x_discrete(labels = function(x) gsub(reg, "", x), ...)
+}
 
-
-
-ui <- fluidPage(
+ui <- navbarPage(
     
-    titlePanel("Game of Thrones Predictions"),
+    title = "Game of Thrones Predictions",
     
-    sidebarLayout(
-        sidebarPanel(
-            selectInput("teams", "Choose Teams:", 
-                        got_points$Team,
-                        selected = "All")
-        ),
-        
-        mainPanel(
-            tableOutput("table")
-        )
+    tabPanel("Leaderboard",
+             sidebarLayout(
+                 sidebarPanel(
+                     selectInput("teams", "Choose Teams:",
+                                 got_points$Team,
+                                 selected = "All")
+                 ),
+                 
+                 mainPanel(
+                     dataTableOutput("leaderboard"),
+                     hr(),
+                     em(paste("Last Updated:", last_updated), align = "right")
+                 )
+             )
+    ),
+    
+    
+    tabPanel("Answers",
+             sidebarLayout(
+                 sidebarPanel(
+                     selectInput("week", "Choose Weeks:",
+                                 c("All", got %>% 
+                                       filter(!is.na(week)) %>% 
+                                       distinct(week) %>% 
+                                       pull()),
+                                 selected = "All")
+                 ),
+                 mainPanel(
+                     dataTableOutput(("answers")),
+                     hr(),
+                     tags$em(paste("Last Updated:", last_updated))
+                 )
+             )
+    ),
+    
+    tabPanel("Submissions",
+             sidebarLayout(
+                 sidebarPanel(
+                     selectInput("individual", "Choose Individual:",
+                                 got_points$Name,
+                                 selected = "Scott")
+                 ),
+                 mainPanel(
+                     dataTableOutput ("submissions") 
+                 )
+             )
+    ),
+
+    tabPanel("Responses",
+             # sidebarLayout(
+             #     sidebarPanel(
+             # selectInput("category", "Choose Category:",
+             #             unique(got$category),
+             #             selected = "character_outcome")
+             #     ),
+             #     mainPanel(
+             #         plotlyOutput(("responses"))
+             #     )
+             # )
+             # plotlyOutput(("responses")),
+             fluidRow(selectInput("category", "Choose Category:",
+                                  unique(got$category),
+                                  selected = "character_outcome"), align = "left"),
+             plotlyOutput(("responses"))
+             
     )
 )
 
 server <- function(input, output) {
     
-    output$table <- renderTable({
+    output$leaderboard <- renderDataTable(
         if(input$teams != "All") {
             got_points %>% 
                 filter(Team %in% c(input$teams, "All")) %>% 
                 select(-Team)
         } else {
             got_points
+        },
+        options = list(pageLength = 20)
+        
+    )
+    
+    output$submissions <- renderDataTable(
+        got %>% 
+            filter(Name == input$individual) %>% 
+            group_by(question) %>% 
+            summarize(answer = paste(answer, collapse = "; ")) %>% 
+            rename(Question = question, Answer = answer)
+        , options = list(pageLength = 100))
+    
+    output$responses <- renderPlotly({
+        
+        if (input$category == "Character Outcome") {
+            
+            p <- got %>% 
+                filter(category == "Character Outcome") %>% 
+                select(Name, question, answer) %>% 
+                replace_na(list(answer = "No Submission")) %>% 
+                mutate(Character = str_extract(question, "\\[.*\\]") %>% 
+                           str_remove_all("\\[|\\]") %>% 
+                           fct_inorder(), 
+                       answer = factor(answer, levels = c("Live", "Die", "Reanimated/Turned", 
+                                                          "Not Seen/Unknown", "No Submission")) %>% 
+                           fct_rev()) %>% 
+                add_count(Character, answer, name = "Guesses") %>% 
+                group_by(Character, answer, Guesses) %>% 
+                summarize(Guessers = paste(Name, collapse = "\n")) %>%
+                ungroup() %>% 
+                ggplot(aes(answer, Guesses, fill = answer, label = Guessers)) + 
+                geom_col() + 
+                scale_fill_manual(values = c("grey35", "#d1c0d8", 
+                                             "#4c94db", "#ea0000", "#2a623d")) + 
+                coord_flip() + 
+                facet_wrap(~ Character) + 
+                theme_minimal() + 
+                labs(x = NULL,
+                     y = "Guesses") + 
+                theme(legend.position='none')
+            
+        } else {
+            
+            p <- got %>% 
+                filter(category == input$category) %>% 
+                select(Name, question, answer) %>% 
+                replace_na(list(answer = "No Submission")) %>% 
+                add_count(answer, name = "Guesses") %>% 
+                group_by(question, answer, Guesses) %>% 
+                summarize(Guessers = paste(Name, collapse = "\n")) %>%
+                ungroup() %>% 
+                mutate(answer = reorder_within(answer, Guesses, question)) %>% 
+                ggplot(aes(answer, Guesses, label = Guessers)) + 
+                geom_col() + 
+                coord_flip() + 
+                scale_x_reordered() + 
+                facet_wrap(~ question, scales = "free", ncol = 2) + 
+                theme_minimal() + 
+                labs(x = NULL,
+                     y = "Guesses")
+            
         }
         
-    }, align = "c", digits = 0)
+        plotly::ggplotly(p, tooltip = c("y", "label"),
+                         height = 800, width = 1200) 
+        
+    })
+    
+    output$answers <- renderDataTable(
+        
+        if(input$week == "All") {
+            
+            got %>% 
+                filter(!is.na(week)) %>% 
+                distinct(Week = week,
+                         Question = question,
+                         Answer = answer_clean,
+                         Points = points)
+            
+        } else {
+            
+            got %>% 
+                filter(week %in% input$week) %>% 
+                distinct(Week = week,
+                         Question = question,
+                         Answer = answer_clean,
+                         Points = points)
+            
+        }
+        
+        , options = list(pageLength = 100))
 }
 
 shinyApp(ui = ui, server = server)
--- got/got-weekly.R
+++ got/got-weekly.R
@@ -1,46 +1,64 @@
 library(googlesheets)
 library(tidyverse)
 
-answers <- gs_title("GOT") %>% 
+responses <- gs_title("GOT") %>% 
   gs_read(ws = 1) %>% 
   rename(Team = `How do you know me?`) %>% 
   select(-Timestamp, -`Email Address`)
 
-points <- gs_title("GOT") %>% 
-  gs_read(ws = 2) %>% 
-  filter(correct == "yes")
+answers <- gs_title("GOT") %>% 
+  gs_read(ws = 2) 
 
-got <- answers %>% 
+# points: 
+# >0 - correct answer
+# =0 - already ruled out (but question still undecided)
+# <0 - incorrect answer
+# NA - undecided
+
+points <- answers %>% 
+  filter(!is.na(points)) %>% 
+  select(-category)
+
+category <- answers %>% 
+  distinct(question, category)
+
+got <- responses %>% 
   gather(question, answer,
          -Name, -Team) %>% 
-  separate_rows(answer, sep = ",") %>% 
+  mutate(answer = str_replace_all(answer, "\\),", "\\);")) %>% 
+  separate_rows(answer, sep = ";") %>% 
   mutate(answer = str_trim(answer)) %>% 
+  left_join(category, by = "question") %>% 
   left_join(points, by = c("question", "answer")) %>%
-  mutate(points = case_when(
-    answer %in% c("Die", "Live",
-                  "Reanimated/Turned", 
-                  "Not Seen/Unknown") ~ 10,
-    !str_detect(answer, "\\d") ~ 50,
-    TRUE ~ str_extract(answer, "(?<=\\().*(?=\\))") %>% # fix this 
-      str_extract("\\d*") %>% 
-      as.numeric))
+  mutate(
+    possible_points = case_when(
+      answer %in% c("Die", "Live",
+                    "Reanimated/Turned", 
+                    "Not Seen/Unknown") ~ 10,
+      !str_detect(answer, "\\d") ~ 50,
+      TRUE ~ str_extract(answer, "\\(\\d.*\\)$") %>% 
+        str_extract("(?<=\\().*(?=\\))") %>% 
+        str_extract("\\d*") %>% 
+        as.numeric()),
+    answer_clean = str_remove(answer, " \\(\\d.*\\)$")
+  )
 
 points_earned <- got %>% 
-  filter(correct == "yes") %>% 
+  filter(!is.na(points)) %>% 
   count(Name, Team, 
         wt = points, name = "Points")
 
 points_remaining <- got %>% 
-  filter(is.na(correct)) %>% 
+  filter(is.na(points)) %>% 
   count(Name, Team, 
-        wt = points, name = "PPR")
+        wt = possible_points, name = "PPR")
 
 got_points <- full_join(points_earned, 
                         points_remaining,
                         by = c("Name", "Team")) %>% 
   replace_na(list(Points = 0,
                   PPR = 0)) %>% 
-  arrange(-Points)
+  arrange(-Points, -PPR)
 
-save(got_points, 
+save(got, got_points, 
      file = here::here("got", "got-data.RData"))
