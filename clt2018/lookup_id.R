158baf8c60fab8b8e6af32f0bd967554abd2e857
Scott Frechette <scottfrechette@gmail.com>
2021-12-14 13:12:51 -0600

    Remove retired apps

--- clt2018/lookup_id.R
+++
@@ -1,5 +0,0 @@
-lookup_id <- tibble(
-  team_id = 1:10,
-  team = c("Barrett", "German", "Bobby", "Scott", "Diaz",
-           "PFinn", "Justin", "David", "Eric", "Brian")
-)
--- sx2018/sx_lookupIDs.R
+++
@@ -1,3 +0,0 @@
-sx_lookup_ids <- data_frame(team_id = c(1, 2, 3, 5, 7, 11, 12, 13), 
-           team = c("Jersey", "Scott", "Burg", "Kral", 
-                    "Ford", "Brazil", "Wikle", "Jangaard"))
